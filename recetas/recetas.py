efcec4979a40ca8a37669e20aeae337a30064dfd
Diego Fernando Marin <dfmarin@gmail.com>
2025-04-29 18:59:38 -0500

    se crean mocks de las funciones

---
+++ recetas/recetas.py
@@ -0,0 +1,14 @@
+# R1: pedir el número de porciones
+def pedir_datos():
+    return -1
+  
+
+# R2: calcular la cantidad de los ingredientes
+def calcular_ingredientes(porciones):
+    return (1.0, 1.0, 1.0, 1.0)
+
+
+# R3: mostrar los ingredientes necesarios
+def mostrar_resultados (porciones, ingredientes):
+    print("ok")
+
